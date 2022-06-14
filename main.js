7d6e1036464a4717b7e8094515488b2681c52e4d
vanessaBonito <vanebonito@hotmail.com>
2022-06-13 20:30:27 -0500

    Fixed eslint errors in main.js

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const menuIcon = document.querySelector('#menu-icon');
 const menuOption = document.getElementsByClassName('menu-option');
 
 function openMenu() {
-    menuNav.classList.toggle('active');
-    menuIcon.classList.toggle('active');
+  menuNav.classList.toggle('active');
+  menuIcon.classList.toggle('active');
 }
 
-function closeMenu(){
-    menuNav.classList.remove('active');
+function closeMenu() {
+  menuNav.classList.remove('active');
 }
 
-menuIcon.onclick = function() {openMenu()};
-menuOption.onclick = function() {closeMenu()};
+menuIcon.onclick = function () { openMenu(); };
+menuOption.onclick = function () { closeMenu(); };
