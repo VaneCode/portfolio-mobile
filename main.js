7678a57cc765b4c6b03f2b691151d16a14e5a057
vanessaBonito <vanebonito@hotmail.com>
2022-06-14 19:33:35 -0500

    Add projects objects

--- main.js
+++ main.js
@@ -13,3 +13,57 @@ function closeMenu() {
 
 menuIcon.onclick = function () { openMenu(); };
 menuOption.onclick = function () { closeMenu(); };
+
+//Projects array objects
+const projects = [
+  {
+    projectID:0,
+    name: 'Project 1',
+    featuredImage: 'images/project_1.jpg',
+    technologies:['HTML/CSS', 'Ruby on rails', 'RubyJavaScript'],
+    linkLiveVersion:'www.google.com',
+    linkSource:'www.github.com'
+  },
+  {
+    projectID:1,
+    name: 'Project 2',
+    featuredImage: 'images/project_2.jpg',
+    technologies:['HTML/CSS', 'Ruby on rails', 'RubyJavaScript'],
+    linkLiveVersion:'www.google.com',
+    linkSource:'www.github.com'
+  },
+  {
+    projectID:2,
+    name: 'Project 3',
+    featuredImage: 'images/project_3.jpg',
+    technologies:['HTML/CSS', 'Ruby on rails', 'RubyJavaScript'],
+    linkLiveVersion:'www.google.com',
+    linkSource:'www.github.com'
+  },
+  {
+    projectID:3,
+    name: 'Project 4',
+    featuredImage: 'images/project_4.jpg',
+    technologies:['HTML/CSS', 'Ruby on rails', 'RubyJavaScript'],
+    linkLiveVersion:'www.google.com',
+    linkSource:'www.github.com'
+  },
+  {
+    projectID:4,
+    name: 'Project 5',
+    featuredImage: 'images/project_5.png',
+    technologies:['HTML/CSS', 'Ruby on rails', 'RubyJavaScript'],
+    linkLiveVersion:'www.google.com',
+    linkSource:'www.github.com'
+  },
+  {
+    projectID:5,
+    name: 'Project 6',
+    featuredImage: 'images/project_6.jpg',
+    technologies:['HTML/CSS', 'Ruby on rails', 'RubyJavaScript'],
+    linkLiveVersion:'www.google.com',
+    linkSource:'www.github.com'
+  },
+ 
+];
+
