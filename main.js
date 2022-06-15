e9ee67c962504a61b2b7e0c0735cfc6a4e0157e1
Vitor Guedes <vgm_rox@hotmail.com>
2022-06-14 22:20:57 -0300

    Add dinamic cards for projects section

--- main.js
+++ main.js
@@ -79,30 +79,44 @@ projects.forEach((project, index) => {
   divProject.appendChild(divProjectImage);
    //Div project name
    let divProjectName = document.createElement('div');
-   let projectName = document.createElement('h2');
+   let projectName = document.createElement('h3');
    let projectNameText = document.createTextNode(project.name);
    projectName.appendChild(projectNameText);
-   divProject.appendChild(projectName);
-  //See project button
-  let btnSeeProject = document.createElement('button');
-  let btnText = document.createTextNode('See project');
-  btnSeeProject.setAttribute("class", "buttons neonText2");
-  btnSeeProject.setAttribute("id", project.projectID);
-  btnSeeProject.appendChild(btnText);
-  divProject.appendChild(btnSeeProject);
-  //Technologis ul
-  let techUl = document.createElement('ul');
-  techUl.setAttribute("class", "skillsused");
-  let techs = project.technologies;
-  techs.forEach((tech) => {
-    let liTech = document.createElement('li');
-    let liText = document.createTextNode(tech);
-    liTech.appendChild(liText);
-    techUl.appendChild(liTech);
-    divProject.appendChild(techUl);
+   divProjectName.appendChild(projectName);
+   divProject.appendChild(divProjectName)
+    //Div for flex technologies
+    let divFlexTech = document.createElement('div');
+    divFlexTech.setAttribute('class', 'flex-ul-work');
+     //Div UL technologies
+     let divUlTech = document.createElement('div');
+     divUlTech.setAttribute('class', "ul-work-item");
+     divFlexTech.appendChild(divUlTech);
+      //Technologis ul
+      let techUl = document.createElement('ul');
+      techUl.setAttribute("class", "ul-style");
+      let techs = project.technologies;
+      techs.forEach((tech) => {
+      let liTech = document.createElement('li');
+      liTech.setAttribute('class', "li-style")
+      let liText = document.createTextNode(tech);
+      liTech.appendChild(liText);
+      techUl.appendChild(liTech);
+      divUlTech.appendChild(techUl);
+      divProject.appendChild(divFlexTech);
+      })
+       //Div for buttoms (project)
+       let divBtnProject = document.createElement('div');
+       let btnProject = document.createElement('button');
+       btnProject.setAttribute('type', "button");
+       btnProject.setAttribute('class', "button-work");
+       let textbtnProject = document.createTextNode('See this project');
+       let svgIcon = document.createElement('svg');
+       svgIcon.setAttribute('class', "arrow_icon flex-button-icon");
+       btnProject.appendChild(svgIcon);
+       btnProject.appendChild(textbtnProject);
+       divBtnProject.appendChild(btnProject);
+       divProject.appendChild(divBtnProject)
+        //All divs inside of the Card div
+        dinamicProjects.appendChild(divProject)
   });
-
-  //Add div to dinamicProjects
-  dinamicProjects.appendChild(divProject);
-});
 
